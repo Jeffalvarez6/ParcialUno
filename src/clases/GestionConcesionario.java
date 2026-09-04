6c4c0997503590abef19cf314afc3fb94b9d4d58
jalvarez77 <jalvarez77@unisalle.edu.co>
2026-09-03 19:41:26 -0500

    Agregar vehiculo

--- src/clases/GestionConcesionario.java
+++ src/clases/GestionConcesionario.java
@@ -11,5 +11,27 @@ import java.util.ArrayList;
  */
 public class GestionConcesionario {
 
+    private ArrayList<Vehiculo> vehiculos;
+    private ArrayList<Venta> ventas;
+
+    /**
+     * Constructor de la clase GestionConcesionario.
+     */
+    public GestionConcesionario() {
+
+        vehiculos = new ArrayList<>();
+        ventas = new ArrayList<>();
+    }
+
+    /**
+     * Agrega un nuevo vehículo al concesionario.
+     *
+     * @param vehiculo vehículo que se desea agregar
+     */
+    public void agregarNuevoVehiculo(Vehiculo vehiculo) {
+
+        vehiculos.add(vehiculo);
+    }
+
    
 }
