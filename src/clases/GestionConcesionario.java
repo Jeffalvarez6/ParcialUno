947e4b948b69758a1f768aeb416e549b5301067c
jalvarez77 <jalvarez77@unisalle.edu.co>
2026-09-03 19:40:08 -0500

    agregar clase GestionConcesionario

---
+++ src/clases/GestionConcesionario.java
@@ -0,0 +1,15 @@
+package clases;
+
+import java.util.ArrayList;
+
+/**
+ * Clase encargada de gestionar los vehículos
+ * disponibles en el concesionario y registrar
+ * las compras realizadas.
+ *
+ * @author Jeferson Alvarez
+ */
+public class GestionConcesionario {
+
+   
+}
